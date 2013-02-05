c28a81a865a832570b2d667b8a646be86e08aca8
bostonlink <bostonlink@pentest-labs.org>
2013-02-05 12:10:23 -0500

    added to peid sig transform

---
+++ src/cuckooforcanari/transforms/to_peid.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from canari.framework import configure
+from canari.maltego.entities import Phrase
+from common.entities import StaticAnalysis, CuckooTaskID, CuckooMalwareFilename
+from common.cuckooapi import report
+from common.cuckooparse import static_results
+
+__author__ = 'bostonlink'
+__copyright__ = 'Copyright 2013, Cuckooforcanari Project'
+__credits__ = []
+
+__license__ = 'GPL'
+__version__ = '0.1'
+__maintainer__ = 'bostonlink'
+__email__ = 'bostonlink@pentest-labs.org'
+__status__ = 'Development'
+
+__all__ = [
+	'dotransform'
+]
+
+@configure(
+	label='To PEID Signature [Cuckoo Sandbox]',
+	description='Returns PEID signature of the malware sample.',
+	uuids=[ 'cuckooforcanari.v2.IDToCuckooPEIDSig_Cuckoo',
+			'cuckooforcanari.v2.FileToCuckooPEIDSig_Cuckoo',
+			'cuckooforcanari.v2.SectionToCuckooPEIDSig_Cuckoo' ],
+	inputs=[ ( 'Cuckoo Sandbox', CuckooTaskID ),
+		( 'Cuckoo Sandbox', CuckooMalwareFilename ),
+		( 'Cuckoo Sandbox', StaticAnalysis ) ],
+	debug=False
+)
+
+def dotransform(request, response):
+
+	if 'taskid' in request.fields:
+		task = request.fields['taskid']
+	else:
+		task = request.value
+
+	secs = static_results(report(task))['peid_signatures']
+	if secs == None:
+		pass
+	else:
+		response += Phrase(secs('ascii'))
+
+	return response
