0c6defe8012c9e5c61833315f903a41b757315f8
bostonlink <bostonlink@pentest-labs.org>
2014-07-02 09:03:09 -0400

    general updates to the package and deleted the requests package

--- src/cuckooforcanari/transforms/common/cuckooparse.py
+++ src/cuckooforcanari/transforms/common/cuckooparse.py
@@ -5,44 +5,44 @@
 
 # Returns dic of analysis information
 def analysis_info(report):
-	return report['info']
+    return report['info']
 
 # Returns list of yara signatures
 def yara_sigs(report):
-	return report['target']['file']['yara']
+    return report['target']['file']['yara']
 
 # Returns list of cuckoo signatures
 def cuckoo_sigs(report):
-	return report['signatures']
+    return report['signatures']
 
 # Returns dic of VT results
 def vt_results(report):
-	return report['virustotal']
+    return report['virustotal']
 
 # Returns dic of networking results
 def network(report):
-	return report['network']
+    return report['network']
 
 # Returns dic of static analysis results
 def static_results(report):
-	return report['static']
+    return report['static']
 
 # Returns list of dropped files
 def dropped_files(report):
-	return report['dropped']
+    return report['dropped']
 
 # Returns dic of behavior results
 def behavior(report):
-	return report['behavior']
+    return report['behavior']
 
 # Returns dic of debug logs for analysis
 def debug_logs(report):
-	return report['debug']
+    return report['debug']
 
 # Returns list of strings from sample
 def strings(report):
-	return report['strings']
+    return report['strings']
 
 # Returns dic of target file results
 def target_info(report):
-	return report['target']
+    return report['target']
--- src/cuckooforcanari/transforms/common/entities.py
+++ src/cuckooforcanari/transforms/common/entities.py
@@ -3,11 +3,11 @@
 from canari.maltego.message import Entity, EntityField
 
 __author__ = 'bostonlink'
-__copyright__ = 'Copyright 2013, Cuckooforcanari Project'
+__copyright__ = 'Copyright 2014, Cuckooforcanari Project'
 __credits__ = []
 
 __license__ = 'GPL'
-__version__ = '0.1'
+__version__ = '1.1'
 __maintainer__ = 'bostonlink'
 __email__ = 'bostonlink@pentest-labs.org'
 __status__ = 'Development'
@@ -34,65 +34,84 @@ __all__ = [
 
 ]
 
+
 @EntityField(name='taskid', propname='taskid', displayname='Task ID')
 class CuckooEntity(Entity):
     namespace = 'cuckoo'
+
 
 @EntityField(name='filename', propname='filename', displayname='Filename')
 @EntityField(name='status', propname='status', displayname='Status')
 class CuckooTaskID(CuckooEntity):
     pass
 
+
 class CuckooSig(CuckooEntity):
     pass
+
 
 @EntityField(name='ftype', propname='ftype', displayname='File Type')
 class CuckooDropped(CuckooEntity):
     pass
-    
+
+
 class CuckooOpenFile(CuckooEntity):
     pass
 
+
 class CuckooMalwareFilename(CuckooEntity):
     pass
+
 
 @EntityField(name='hashtype', propname='hashtype', displayname='Hash Type')
 class CuckooHash(CuckooEntity):
     pass
 
+
 @EntityField(name='vtlink', propname='vtlink', displayname='VT Link')
 class CuckooVT(CuckooEntity):
     pass
 
+
 class CuckooYara(CuckooEntity):
     pass
+
 
 class CuckooMalwareSample(CuckooEntity):
     pass
 
+
 class CuckooMutex(CuckooEntity):
     pass
+
 
 class CuckooProcess(CuckooEntity):
     pass
 
+
 class FileDetails(CuckooEntity):
     pass
+
 
 class SignatureAnalysis(CuckooEntity):
     pass
 
+
 class NetworkAnalysis(CuckooEntity):
     pass
+
 
 class StaticAnalysis(CuckooEntity):
     pass
 
+
 class DroppedFiles(CuckooEntity):
     pass
 
+
 class BehaviorAnalysis(CuckooEntity):
     pass
+
 
 class Processes(CuckooEntity):
     pass
